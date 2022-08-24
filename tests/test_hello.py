6173cde665b9f1aeccb96c136ce0a95844f7dacb
SCHILD Erwan <erwan.schild@e-i.com>
2022-08-24 13:35:44 +0200

    refactor: remove MVP app

--- tests/test_hello.py
+++
@@ -1,8 +0,0 @@
-"""Tests for the `hello` module."""
-
-from cognitivefactory.interactive_clustering_gui import hello
-
-
-def test_hello():
-    """Test that the `hello.hello_world` method works."""
-    assert hello.hello_world() == "Hello World !"
